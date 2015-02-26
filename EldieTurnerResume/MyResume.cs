3cdb72ecaa7460a27237c47a564bb121fdc81b3a
EldieTurner <Eldie.Turner@live.com>
2015-02-26 16:44:46 -0500

    Initial commit of Eldie Turner C# Resume. 2/26/2015

---
+++ EldieTurnerResume/MyResume.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections.Generic;
+using Duty = System.String;
+using skill = System.String;
+
+class MyResume
+{
+    static void Main()
+    {
+        var resume = new Resume();
+
+        resume.Title = "Senior .NET Developer";
+
+        resume.ContactInfo = new ContactInfo()
+        {
+                Name = "Eldie Turner",
+                Phone = "440.678.8268",
+                Email = "Eldie.Turner@live.com",
+                Website = "http://EldieTurner.com",
+                LinkedIn = "https://www.linkedin.com/in/eldieturner",
+                Twitter = "https://twitter.com/eldieturner" //I don't really tweet
+        };
+
+        resume.WorkExperiences = new List<WorkExperience>()
+        {
+            new WorkExperience()
+            {
+                CompanName = "Onosys (a LivingSocial Company)",
+                From = "06/2014", To = "Present",
+                Title = "Senior .NET Developer",
+                Duties = new List<Duty>()
+                {
+                    "• Perform bug fixes for existing applications (MVC, and Web API)",
+                    "• Help create new features for existing applications",
+                    "• Write and update unit tests",
+                    "• Perform front end work for product, using html5, css3, JQuery and AngularJS",
+                    "• Work with data in SQL Server 2012/1014 as well as RavenDB and MongoDB"
+                }
+            },
+
+            new WorkExperience()
+            {
+                CompanName = "The Cleveland Clinic",
+                From = "03/2013", To = "06/2014",
+                Title = "Web Development Manager",
+                Duties = new List<Duty>()
+                {
+                    "• Design Databases within SQL Server 2008R2/2012",
+                    "• Design new .NET applications with frameworks (4.5 and 4.5.1)",
+                    "• Build new applications with C# and MVC 4/5",
+                    "• Use Entity Framework 5/6 with both Database First and Code First",
+                    "• Use JQuery and WCF services to create responsive, light weight web applications.",
+                    "• Design secure login process (password management, password reset loop)",
+                    "• Design auditing system for login, data changes, and data view",
+                    "• Mentor junior developers",
+                    "• Implement (Test Driven Design) TDD and Unit Tests where appropriate"
+                }
+            },
+
+            new WorkExperience()
+            {
+                CompanName = "SummaCare",
+                ContratingCompany = "Randstad Technologies",
+                From = "09/2012", To = "03/2013",
+                Title = "Senior .NET Developer",
+                Duties = new List<Duty>()
+                {
+                    "• Modify existing .NET4.0  applications",
+                    "• Work with SQL Server 2008R2 and Oracle databases",
+                    "• Modify SSIS packages",
+                    "• Modify stored procedures",
+                    "• Work with project managers to determine requirements and timeline",
+                    "• Write turnover documents and update BA specs for programs",
+                    "• Do 100% of application testing"
+                }
+            },
+
+            new WorkExperience()
+            {
+                CompanName = "Westfield Insurance",
+                ContratingCompany = "TekSystems",
+                From = "02/2012", To = "08/2012",
+                Title = "Test Automation Specialist",
+                Duties = new List<Duty>()
+                {
+                    "• Develop an ASP.NET application for internal use",
+                    "• Utilize Jquery, AJAX.NET, and Entity Framework in new application",
+                    "• Worked in an AGILE environment",
+                    "• Responsible for application through entire SDLC",
+                    "• Design SQL Server Database",
+                    "• SQL Server Administrator",
+                    "• IIS 7 Administrator",
+                    "• Windows Server 2008R2 Administrator"
+                }
+            },
+
+            new WorkExperience()
+            {
+                CompanName = "NASA (National Aeronautics and Space Administration)",
+                ContratingCompany = "ASRC (Arctic Slope Regional Corporation)",
+                From = "08/2006", To = "02/2012",
+                Title = "Program Analyst IV",
+                Duties = new List<Duty>()
+                {
+                    "• Lead Programmer on 2 contract management applications (ASP.NET)",
+                    "• Maintain several ASP.NET (C# and VB) applications using Visual Studio (2003,2008 and 2010)",
+                    "• Wrote and consumed WCF (Windows Communication Foundation) services",
+                    "• Worked in an AGILE environment",
+                    "• Responsible for applications through entire SDLC",
+                    "• Develop .NET applications on top of Sharepoint 2008",
+                    "• Attend requirements meetings for multiple applications",
+                    "• Helped to design application look and features, including page layout, workflow, and database strictures",
+                    "• Write SQL statements for getting data into and out of the database",
+                    "• Create custom data objects with ADO.NET for both Access and SQL Server",
+                    "• Write test plans for QA department to do testing",
+                    "• Do black box testing for application changes",
+                    "• Web Administrator on 3 Windows 2003/2008-R2 Web Servers (IIS 6 and IIS7)",
+                    "• Server Administrator on 2 Windows 2003/2008-R2 File Servers",
+                    "• Server admin on MS SQL Server 2008",
+                    "• Assisted with IT security plans, and implemented harder security in applications and on servers",
+                    "• Help create and implement disaster recovery plans"
+                }
+            },
+        };
+
+        resume.TechnicalSkills  = new List<skill>()
+        {
+            "dot net framework 1.1 - 4.5.2", "asp.net mvc", ".net Web api", "c#",
+            "object oriented programming (oop)", "design patterns", "functional programming",
+            "mongoDB", "ravenDB", "Microsoft SQL Server 2005-2012", "T-SQL", "stored procedures", "ssis",
+            "javascript", "jquery", "knockout", "angularjs", "css", "html5"
+        };
+
+        resume.Education = new List<Degree>()
+        {
+            new Degree()
+            {
+                School = "Lakeland Community College",
+                Type = "Associate of Art",
+                Year = "2001"
+            }
+        };
+        resume.Close();
+    }
+}
---
+++ EldieTurnerResume/Resume.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Resume
+{
+    public Resume() { }
+    public string Title { get; set; }
+    public ContactInfo ContactInfo {get;set;}
+    public List<WorkExperience> WorkExperiences { get; set; }
+    public List<string> TechnicalSkills { get; set; }
+    public List<Degree> Education { get; set; }
+
+    public void Close()
+    {
+        center(this.Title);
+        lIne();
+        center(this.ContactInfo.Name);
+        center(this.ContactInfo.Phone);
+        center(this.ContactInfo.Email);
+        center(this.ContactInfo.Website);
+        center(this.ContactInfo.LinkedIn);
+        center(this.ContactInfo.Twitter);
+        Console.ReadLine();
+
+        foreach (var work in WorkExperiences)
+        {
+            clear();
+            write(work.Title);
+            lIne();
+            write("Company: " + work.CompanName);
+            write("Years :" + work.From + " - " + work.To);
+            foreach(var duty in work.Duties)
+                indent(duty);
+            Console.ReadLine();
+        }
+        clear();
+        write("Technical Skills:");
+        TechnicalSkills.ForEach(x => indent(x + ", "));
+        Console.ReadLine();
+        clear();
+        foreach (var degree in Education)
+        {
+            write(degree.School);
+            indent(degree.Type +  " " + degree.Year);
+        }
+        Console.ReadLine();
+    }
+
+    public static void write(string text)
+    {
+        Console.WriteLine(text);
+    }
+
+    public static void clear()
+    {
+        Console.Clear();
+    }
+
+    private static void center(String text)
+    {
+        Console.Write(new string(' ', (Console.WindowWidth - text.Length) / 2));
+        write(text);
+    }
+
+    private static void lIne(int LineCount = 1)
+    {
+        for (int i = 1; i <= LineCount; i++)
+            write("");
+    }
+
+    private static void indent(String text, int indentcount = 1)
+    {
+        for (int i = 1; i <= indentcount; i++)
+            Console.Write(new string(' ', 3));
+        write(text);
+    }
+}
+
+class ContactInfo
+{
+    public string Name { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Website { get; set; }
+    public string LinkedIn { get; set; }
+    public string Twitter { get; set; }
+}
+
+class WorkExperience
+{
+    public string CompanName { get; set; }
+    public string ContratingCompany { get; set; }
+    public string From { get; set; }
+    public string To { get; set; }
+    public string Title { get; set; }
+    public List<string> Duties { get; set; }
+}
+
+class Degree
+{
+    public string School { get; set; }
+    public string Type { get; set; }
+    public string Year { get; set; }
+}
